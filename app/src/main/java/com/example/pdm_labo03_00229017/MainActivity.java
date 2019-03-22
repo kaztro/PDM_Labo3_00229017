3d30db80eb4dbefe5e207894fa81356e75204483
kaztro <00229017@uca.edu.sv>
2019-03-22 17:43:32 -0600

    XML finished, starting the .java doc.

--- app/src/main/java/com/example/pdm_labo03_00229017/MainActivity.java
+++ app/src/main/java/com/example/pdm_labo03_00229017/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.pdm_labo03_00229017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_user, tv_pass, tv_mail, tv_gender;
+    EditText et_user, et_pass, et_mail, et_gender;
+    Button btn_send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TextViews
+        tv_user = findViewById(R.id.tv_username_id);
+        tv_pass = findViewById(R.id.tv_pass);
+        tv_mail = findViewById(R.id.tv_mail);
+        tv_gender = findViewById(R.id.tv_gender);
+        //EditTexts
+        et_user = findViewById(R.id.et_user);
+        et_pass = findViewById(R.id.et_pass);
+        et_mail = findViewById(R.id.et_mail);
+        et_gender = findViewById(R.id.et_gender);
+        //Button
+        btn_send = findViewById(R.id.btn_send);
     }
 }
