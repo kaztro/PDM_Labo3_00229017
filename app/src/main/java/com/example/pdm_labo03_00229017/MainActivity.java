0e13e27fff46019c2a59a179dc6369b5f733a5a2
kaztro <00229017@uca.edu.sv>
2019-03-25 10:06:50 -0600

    It's complete.

--- app/src/main/java/com/example/pdm_labo03_00229017/MainActivity.java
+++ app/src/main/java/com/example/pdm_labo03_00229017/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.pdm_labo03_00229017;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.pdm_labo03_00229017.utils.AppConstants;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,5 +31,14 @@ public class MainActivity extends AppCompatActivity {
         et_gender = findViewById(R.id.et_gender);
         //Button
         btn_send = findViewById(R.id.btn_send);
+
+        btn_send.setOnClickListener(v->{
+            Intent mIntent = new Intent(MainActivity.this, ShareActivity.class);
+            mIntent.putExtra(AppConstants.TEXT_KEY, et_user.getText().toString());
+            mIntent.putExtra(AppConstants.TEXT_KEY_1, et_pass.getText().toString());
+            mIntent.putExtra(AppConstants.TEXT_KEY_2, et_mail.getText().toString());
+            mIntent.putExtra(AppConstants.TEXT_KEY_3, et_gender.getText().toString());
+            startActivity(mIntent);
+        });
     }
 }
---
+++ app/src/main/java/com/example/pdm_labo03_00229017/utils/AppConstants.java
@@ -0,0 +1,8 @@
+package com.example.pdm_labo03_00229017.utils;
+
+public class AppConstants {
+    public static final String TEXT_KEY = "textKey";
+    public static final String TEXT_KEY_1 = "textKey_1";
+    public static final String TEXT_KEY_2 = "textKey_2";
+    public static final String TEXT_KEY_3 = "textKey_3";
+}
